babd9d3cdf9d0a6dc6492b121bd4cfadd44c36bf
gutoodc2 <gustavo.copini@gmail.com>
2017-09-28 11:32:08 -0300

    começando grafico

--- Trabalho_M1/src/trabalho_m1/MeuDesempenhoController.java
+++ Trabalho_M1/src/trabalho_m1/MeuDesempenhoController.java
@@ -11,6 +11,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.ComboBox;
 
 
@@ -23,20 +27,37 @@ public class MeuDesempenhoController extends InterfaceUsuario {
     @FXML
     private ComboBox disciplinasBox;
     
+    @FXML
+    private BarChart<?,?> grafico;
+    
+    @FXML
+    private CategoryAxis x;
+    
+    @FXML
+    private NumberAxis y;
+    
     ObservableList<String> disciplinasList = FXCollections
                           .observableArrayList("Arquitetura de Computadores II",
                                                 "Programação Orientada a Obejtos");
-    
+ 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         disciplinasBox.setValue("Selecione a Disciplina");
         disciplinasBox.setItems(disciplinasList);
+        
     }
     
     @FXML
     public void mostrarDesempenho(ActionEvent evento){
         
         
+        XYChart.Series setGrafico = new XYChart.Series();
+        
+        setGrafico.getData().add(new XYChart.Data("M1", 9));
+        setGrafico.getData().add(new XYChart.Data("M2", 5));
+        setGrafico.getData().add(new XYChart.Data("M3", 7));
+        setGrafico.getData().add(new XYChart.Data("MF", 6));
+        grafico.getData().addAll(setGrafico);
         
     }
     
